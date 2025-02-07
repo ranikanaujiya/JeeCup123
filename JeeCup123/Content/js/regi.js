801c89f96b300e822649de852b49fd6db335e93e
ranikanaujiya <ranikanaujiya98gmail.com>
2025-02-07 09:42:35 +0530

    Add project files.

---
+++ JeeCup123/Content/js/regi.js
@@ -0,0 +1,59 @@
+﻿function SubmitFrom() {
+
+    var name = $("#name").val();
+    var father = $("#father").val();
+    var mother = $("#mother").val();
+    var dob = $("#dob").val();
+    var gender = $("#gender").val();
+
+    debugger;
+    if (name == null || name == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "Name is Required!",
+
+        });
+        return;
+
+    }
+    if (father == null || father == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "Father Name is Required!",
+
+        });
+        return;
+
+    }
+    if (mother == null || mother == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: " Mother Name is Required!",
+
+        });
+        return;
+
+    }
+    if (dob == null || dob == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "warning...",
+            text: "dob is Required!",
+
+        });
+        return;
+
+    }
+    if (gender == null || gender == "") {
+        Swal.fire({
+            icon: "warning",
+            title: "Warning...",
+            text: "Gender is required!",
+        });
+        return;
+    }
+
+}
---
+++ JeeCup123/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JeeCup123.MvcApplication" Language="C#" %>
